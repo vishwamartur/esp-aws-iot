b0dd3ba87ec2c54e45935402a543a99bf0f91a1b
kshitij.patil <kshitij.patil@espressif.com>
2024-03-21 18:06:50 +0530

    Network Transport: APIs to set timeouts for MQTT connect, send and receive.

--- libraries/coreMQTT/port/network_transport/network_transport.c
+++ libraries/coreMQTT/port/network_transport/network_transport.c
@@ -11,6 +11,23 @@
 
 #define TAG "network_transport"
 
+Timeouts_t timeouts = { .connectionTimeoutMs = 4000, .sendTimeoutMs = 10000, .recvTimeoutMs = 2000 };
+
+void vTlsSetConnectTimeout( uint16_t connectionTimeoutMs )
+{
+    timeouts.connectionTimeoutMs = connectionTimeoutMs;
+}
+
+void vTlsSetSendTimeout( uint16_t sendTimeoutMs )
+{
+    timeouts.sendTimeoutMs = sendTimeoutMs;
+}
+
+void vTlsSetRecvTimeout( uint16_t recvTimeoutMs )
+{
+    timeouts.recvTimeoutMs = recvTimeoutMs;
+}
+
 TlsTransportStatus_t xTlsConnect( NetworkContext_t* pxNetworkContext )
 {
     TlsTransportStatus_t xResult = TLS_TRANSPORT_CONNECT_FAILURE;
@@ -26,11 +43,11 @@ TlsTransportStatus_t xTlsConnect( NetworkContext_t* pxNetworkContext )
         .ds_data = pxNetworkContext->ds_data,
         .clientkey_buf = ( const unsigned char* )( pxNetworkContext->pcClientKey ),
         .clientkey_bytes = pxNetworkContext->pcClientKeySize,
-        .timeout_ms = 2000,
+        .timeout_ms = timeouts.connectionTimeoutMs,
         .non_block = false,
     };
 
-    if( xSemaphoreTake(pxNetworkContext->xTlsContextSemaphore, portMAX_DELAY) == pdTRUE )
+    if( xSemaphoreTake( pxNetworkContext->xTlsContextSemaphore, portMAX_DELAY ) == pdTRUE )
     {
         int lConnectResult = -1;
         esp_tls_t * pxTls = esp_tls_init();
@@ -46,13 +63,12 @@ TlsTransportStatus_t xTlsConnect( NetworkContext_t* pxNetworkContext )
 
             if( lConnectResult == 1 )
             {
-
                 int lSockFd = -1;
-                if( esp_tls_get_conn_sockfd(pxNetworkContext->pxTls, &lSockFd) == ESP_OK)
+                if( esp_tls_get_conn_sockfd( pxNetworkContext->pxTls, &lSockFd ) == ESP_OK )
                 {
-                    int flags = fcntl(lSockFd, F_GETFL);
+                    int flags = fcntl( lSockFd, F_GETFL );
 
-                    if( fcntl(lSockFd, F_SETFL, flags | O_NONBLOCK ) != -1)
+                    if( fcntl( lSockFd, F_SETFL, flags | O_NONBLOCK ) != -1 )
                     {
                         xResult = TLS_TRANSPORT_SUCCESS;
                     }
@@ -63,6 +79,8 @@ TlsTransportStatus_t xTlsConnect( NetworkContext_t* pxNetworkContext )
             {
                 esp_tls_conn_destroy( pxNetworkContext->pxTls );
                 pxNetworkContext->pxTls = NULL;
+            } else 
+            {
             }
         }
         ( void ) xSemaphoreGive( pxNetworkContext->xTlsContextSemaphore );
@@ -81,7 +99,7 @@ TlsTransportStatus_t xTlsDisconnect( NetworkContext_t* pxNetworkContext )
         {
             xResult = TLS_TRANSPORT_SUCCESS;
         }
-        else if(esp_tls_conn_destroy(pxNetworkContext->pxTls ) == 0)
+        else if( esp_tls_conn_destroy(pxNetworkContext->pxTls ) == 0)
         {
             xResult = TLS_TRANSPORT_SUCCESS;
         }
@@ -101,7 +119,7 @@ TlsTransportStatus_t xTlsDisconnect( NetworkContext_t* pxNetworkContext )
 }
 
 int32_t espTlsTransportSend( NetworkContext_t* pxNetworkContext,
-                             const void* pvData, size_t uxDataLen)
+                             const void* pvData, size_t uxDataLen )
 {
     int32_t lBytesSent = -1;
 
@@ -111,18 +129,19 @@ int32_t espTlsTransportSend( NetworkContext_t* pxNetworkContext,
         ( pxNetworkContext->pxTls != NULL ) )
     {
         TimeOut_t xTimeout;
-        TickType_t xTicksToWait = pdMS_TO_TICKS(10);
-
         vTaskSetTimeOutState( &xTimeout );
+
+        struct timeval timeout = { .tv_usec = timeouts.sendTimeoutMs * 1000, .tv_sec = 0 };
+        TickType_t xTicksToWait = pdMS_TO_TICKS( timeouts.sendTimeoutMs );
+        TickType_t start_tick = xTaskGetTickCount();
 
         if( xSemaphoreTake( pxNetworkContext->xTlsContextSemaphore, xTicksToWait ) == pdTRUE )
         {
             int lSockFd = -1;
             esp_err_t xError = esp_tls_get_conn_sockfd( pxNetworkContext->pxTls, &lSockFd );
-            if( xError == ESP_OK)
+            if( xError == ESP_OK )
             {
                 unsigned char * pucData = ( unsigned char * ) pvData;
-                struct timeval timeout = { .tv_usec = 10000, .tv_sec = 0 };
                 lBytesSent = 0;
                 do
                 {
@@ -132,25 +151,25 @@ int32_t espTlsTransportSend( NetworkContext_t* pxNetworkContext,
 
                     FD_ZERO( &write_fds );
                     FD_SET( lSockFd, &write_fds );
-
                     FD_ZERO( &error_fds );
                     FD_SET( lSockFd, &error_fds );
 
+                    suseconds_t elapsed_time_usec = ( xTaskGetTickCount() - start_tick ) * portTICK_PERIOD_MS * 1000;
+                    timeout.tv_usec = ( timeout.tv_usec - elapsed_time_usec >= 0 ) ? timeout.tv_usec - elapsed_time_usec : 0;
                     lSelectResult = select( lSockFd + 1, NULL, &write_fds, &error_fds, &timeout );
 
                     if( lSelectResult < 0 )
                     {
                         lBytesSent = -1;
-                        ESP_LOGE(TAG, "Error during call to select.");
-                        break;
+                        ESP_LOGE( TAG, "Error during call to select." );
                     }
                     else if( ( lSelectResult > 0 ) && ( FD_ISSET( lSockFd, &write_fds ) != 0 ) )
                     {
                         ssize_t lResult = esp_tls_conn_write( pxNetworkContext->pxTls,
-                                                        &(pucData[lBytesSent]),
+                                                        &( pucData[lBytesSent] ),
                                                      uxDataLen - lBytesSent );
 
-                        if( lResult > 0 )
+                        if( lResult >= 0 )
                         {
                             lBytesSent += ( int32_t ) lResult;
                         }
@@ -161,22 +180,17 @@ int32_t espTlsTransportSend( NetworkContext_t* pxNetworkContext,
                         }
                         else
                         {
-                            /* Empty when lResult == 0 */
-                        }
-
-                        if( ( lBytesSent < 0 ) ||
-                            ( lBytesSent == uxDataLen ) )
-                        {
-                            break;
+                            /* Empty when MBEDTLS_ERR_SSL_WANT_READ || MBEDTLS_ERR_SSL_WANT_WRITE */
                         }
                     }
-
                     else
                     {
                         /* Empty when lSelectResult == 0 */
                     }
                 }
-                while( xTaskCheckForTimeOut( &xTimeout, &xTicksToWait ) == pdFALSE );
+                while( ( xTaskCheckForTimeOut( &xTimeout, &xTicksToWait ) == pdFALSE ) &&
+                       ( lBytesSent < uxDataLen ) &&
+                       ( lBytesSent >= 0 ) );
             }
             xSemaphoreGive(pxNetworkContext->xTlsContextSemaphore);
         }
@@ -186,38 +200,74 @@ int32_t espTlsTransportSend( NetworkContext_t* pxNetworkContext,
 }
 
 int32_t espTlsTransportRecv( NetworkContext_t* pxNetworkContext,
-                             void* pvData, size_t uxDataLen)
+                             void* pvData, size_t uxDataLen )
 {
-    int32_t lBytesRead = -1;
+    int32_t lBytesRead = 0;
 
     if( ( pvData != NULL ) &&
         ( uxDataLen > 0 ) &&
         ( pxNetworkContext != NULL ) &&
         ( pxNetworkContext->pxTls != NULL ) )
     {
-        if( xSemaphoreTake( pxNetworkContext->xTlsContextSemaphore, portMAX_DELAY ) == pdTRUE )
+        TimeOut_t xTimeout;
+        vTaskSetTimeOutState( &xTimeout );
+
+        TickType_t xTicksToWait = pdMS_TO_TICKS( timeouts.recvTimeoutMs );
+        struct timeval timeout = {.tv_usec = timeouts.recvTimeoutMs * 1000, .tv_sec = 0};
+        TickType_t start_tick = xTaskGetTickCount();
+
+        if( xSemaphoreTake( pxNetworkContext->xTlsContextSemaphore, xTicksToWait ) == pdTRUE )
         {
-            ssize_t lResult = esp_tls_conn_read( pxNetworkContext->pxTls,
-                                            pvData,
-                                        ( size_t ) uxDataLen );
+            int lSockFd = -1;
+            fd_set read_fds;
+            fd_set error_fds;
 
-            if( lResult > 0 )
-            {
-                lBytesRead = ( int32_t ) lResult;
-            }
-            else if( ( lResult != MBEDTLS_ERR_SSL_WANT_WRITE ) &&
-                     ( lResult != MBEDTLS_ERR_SSL_WANT_READ ) )
-            {
-                lBytesRead = ( int32_t ) lResult;
-            }
-            else
-            {
-                lBytesRead = 0;
-            }
+            esp_tls_get_conn_sockfd( pxNetworkContext->pxTls, &lSockFd );
+            FD_ZERO( &read_fds );
+            FD_SET( lSockFd, &read_fds );
+            FD_ZERO( &error_fds );
+            FD_SET( lSockFd, &error_fds );
 
-            ( void ) xSemaphoreGive( pxNetworkContext->xTlsContextSemaphore);
+            do
+            {
+                // Fetch any buffered data
+                ssize_t lResult = esp_tls_conn_read( pxNetworkContext->pxTls,
+                                                pvData,
+                                            ( size_t ) uxDataLen );
+
+                if( lResult > 0 )
+                {
+                    lBytesRead = ( int32_t ) lResult;
+                }
+                else if( ( lResult == MBEDTLS_ERR_SSL_WANT_WRITE ) ||
+                         ( lResult == MBEDTLS_ERR_SSL_WANT_READ ) )
+                {
+                    suseconds_t elapsed_time_usec = ( xTaskGetTickCount() - start_tick ) * portTICK_PERIOD_MS * 1000;
+                    timeout.tv_usec = ( timeout.tv_usec - elapsed_time_usec >= 0 ) ? timeout.tv_usec - elapsed_time_usec : 0;
+
+                    int lSelectResult = select( lSockFd + 1, &read_fds, NULL, &error_fds, &timeout );
+                    if ( ( lSelectResult < 0 ) || FD_ISSET( lSockFd, &error_fds ) ) {
+                        ESP_LOGE( TAG, "Error reading the message" );
+                        lBytesRead = lResult = -1;
+                    }
+                }
+                else if( lResult == 0 )
+                {
+                    ESP_LOGE( TAG, "Connection closed" );
+                    lBytesRead = -1;
+                }
+                else
+                {
+                    ESP_LOGE( TAG, "Error reading: %d", ( int ) lResult );
+                    lBytesRead = ( int32_t ) lResult;
+                }
+            }
+            while ( ( xTaskCheckForTimeOut( &xTimeout, &xTicksToWait ) == pdFALSE ) &&
+                    ( lBytesRead == 0 ) );
+
+
+            ( void ) xSemaphoreGive( pxNetworkContext->xTlsContextSemaphore );
         }
     }
-
     return lBytesRead;
 }
